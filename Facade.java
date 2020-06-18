1527a4423fbe22ed2a0f6bce890f0df7d40f321b
Gordito developer <sneider.developer@gmail.com>
2020-06-18 17:30:02 -0500

    start

--- Facade.java
+++ Facade.java
@@ -2,15 +2,10 @@ public class Facade{
 
     public static void main(String[] args) {
         System.out.println("It time");
-        System.out.println("Este es el cambio");
-
-        System.out.println("Este es el cambio");
 
         //git pull origin master - trae cambios
-        //git push - empuja cambios
+        //git push origin master - empuja cambios
         //git add <file> - pasar a la rama state
         // git add -A - todos los archivos
-        System.out.println("Futbol");
-
     }
 }
