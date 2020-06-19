de1441f670da4ecc758e9e54340cce147a4f8b74
Gordito developer <sneider.developer@gmail.com>
2020-06-19 16:53:11 -0500

    Construyendo clase Falcon9

--- Falcon9.java
+++ Falcon9.java
@@ -2,4 +2,22 @@ public class Falcon9 {
     public MotorMerlin motorMerlin;
     public Combustible combustible;
     public Posicion posicion;
+
+    public Falcon9(MotorMerlin motorMerlin, Combustible combustible, Posicion posicion){
+        this.motorMerlin = motorMerlin;
+        this.combustible = combustible;
+        this.posicion = posicion;
+    }
+
+    public MotorMerlin gMotorMerlin() {
+        return motorMerlin;
+    }
+
+    public Combustible gCombustible() {
+        return combustible;
+    }
+
+    public Posicion gPosicion() {
+        return posicion;
+    }
 }
