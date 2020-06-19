d486133055667ad3aa550bd55451ff91cc6a2f09
Gordito developer <sneider.developer@gmail.com>
2020-06-19 17:15:37 -0500

    Modificando clase Falcon9

--- Falcon9.java
+++ Falcon9.java
@@ -1,9 +1,15 @@
 public class Falcon9 {
+
     public MotorMerlin motorMerlin;
     public Combustible combustible;
     public Posicion posicion;
+    public EstadoMotor estadoMotor;
 
-    public Falcon9(MotorMerlin motorMerlin, Combustible combustible, Posicion posicion){
+    public Falcon9(MotorMerlin motorMerlin, 
+                    Combustible combustible, 
+                    Posicion posicion, 
+                    EstadoMotor estadoMotor){
+
         this.motorMerlin = motorMerlin;
         this.combustible = combustible;
         this.posicion = posicion;
@@ -19,5 +25,9 @@ public class Falcon9 {
 
     public Posicion gPosicion() {
         return posicion;
+    }
+
+    public EstadoMotor gEstadoMotor() {
+        return estadoMotor;
     }
 }
