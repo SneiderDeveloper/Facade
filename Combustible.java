2a64a7ccaef46ae310dd3257d1cc6dffb49cd957
Gordito developer <sneider.developer@gmail.com>
2020-06-19 17:06:14 -0500

    Modificando clase Combustible

--- Combustible.java
+++ Combustible.java
@@ -10,4 +10,21 @@ public class Combustible {
     public void verificarOxigenoLiquido(float nivelOxigenoLiquido) {
         this.oxigeno = nivelOxigenoLiquido;
     }
+
+    public boolean verificarEstadoCombustible() {
+        boolean listo = false;
+        if (queroseno == 100.0){
+            listo = !listo;
+        } else {
+            System.out.println("Tanque de queroseno en: " + queroseno);
+        }
+
+        if (oxigeno == 100.0) {
+            listo = !listo;
+        } else {
+            System.out.println("Tanque de oxigeno en: " + oxigeno);
+        }
+
+        return listo;
+    }
 }
