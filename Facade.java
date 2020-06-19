aae33e476da02a718d2997373986bf506a197477
Gordito developer <sneider.developer@gmail.com>
2020-06-19 11:26:41 -0500

    modificando clases

--- Facade.java
+++ Facade.java
@@ -1,11 +1,12 @@
-public class Facade{
+public class Facade {
 
-    public static void main(String[] args) {
-        System.out.println("It time");
+    public void listo() {
 
-        //git pull origin master - trae cambios
-        //git push origin master - empuja cambios
-        //git add <file> - pasar a la rama state
-        // git add -A - todos los archivos
     }
+
+    public void abortar() {
+        
+    }
+    
+
 }
