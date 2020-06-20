63789795f99efdece03374c3da58b7bee82534fd
Gordito developer <sneider.developer@gmail.com>
2020-06-20 16:13:44 -0500

    Modificando clase Facade

--- Facade.java
+++ Facade.java
@@ -1,14 +1,23 @@
 public class Facade {
 
     Combustible combustible = new Combustible();
+    EstadoMotor estadoMotor = new EstadoMotor();
+    Posicion posicion = new Posicion();
+    Falcon9 falconNine = new Falcon9(combustible, posicion, estadoMotor);
+    
 
     public void listo() {
-        combustible.verificarQueroseno(100.0f);
-        combustible.verificarOxigenoLiquido(100.0f);
-        boolean combustibleListo = combustible.verificarEstadoCombustible();
-        System.out.println(combustibleListo);
+        falconNine.gCombustible().obtenerQueroseno(100.0);
+        falconNine.gCombustible().obtenerOxidante(100.0);
+        boolean combustibleListo = falconNine.gCombustible().verificarEstadoCombustible();
+        System.out.println("Estado combustible: " + combustibleListo);
+       
+        boolean motorListo = falconNine.gEstadoMotor().verificarEstado();
+        System.out.println("Estado motores: " + motorListo);
 
-        
+        falconNine.gPosicion().obtenerGravitacion(0.0);
+        boolean posicionCorrecta = falconNine.gPosicion().verificarInclinacion();
+        System.out.println("Estado posición: " + posicionCorrecta);
     }
 
     public void abortar() {
