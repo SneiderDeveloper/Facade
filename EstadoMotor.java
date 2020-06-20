27db29d5b0fea36f3ea94cbc8c706e09854c6b4c
Gordito developer <sneider.developer@gmail.com>
2020-06-20 16:12:57 -0500

    Modificando clase EstadoMotor

--- EstadoMotor.java
+++ EstadoMotor.java
@@ -1,5 +1,7 @@
 public class EstadoMotor {
 
+    // MotorMerlin objetos[] = new MotorMerlin[8];
+
     MotorMerlin merlinUno = new MotorMerlin();
     MotorMerlin merlinDos = new MotorMerlin();
     MotorMerlin merlinTres = new MotorMerlin();
@@ -9,27 +11,62 @@ public class EstadoMotor {
     MotorMerlin merlinSiete = new MotorMerlin();
     MotorMerlin merlinOcho = new MotorMerlin();
     MotorMerlin merlinNueve = new MotorMerlin();
+
+    MotorMerlin objetos[] = {
+        merlinUno,
+        merlinDos,
+        merlinTres,
+        merlinCuatro,
+        merlinCinco,
+        merlinSeis,
+        merlinSiete,
+        merlinOcho,
+        merlinNueve
+};
     
     public boolean verificarEstado() {
 
+        merlinUno.obtenerTemperatura(70.7);
+        merlinUno.obtenerPresion(9.7);
+
+        merlinDos.obtenerTemperatura(70.7);
+        merlinDos.obtenerPresion(9.7);
+
+        merlinTres.obtenerTemperatura(70.7);
+        merlinTres.obtenerPresion(9.7);
+
+        merlinCuatro.obtenerTemperatura(70.7);
+        merlinCuatro.obtenerPresion(9.7);
+
+        merlinCinco.obtenerTemperatura(100.0);
+        merlinCinco.obtenerPresion(9.7);
+
+        merlinSeis.obtenerTemperatura(70.7);
+        merlinSeis.obtenerPresion(9.7);
+
+        merlinSiete.obtenerTemperatura(70.7);
+        merlinSiete.obtenerPresion(9.7);
+
+        merlinOcho.obtenerTemperatura(70.7);
+        merlinOcho.obtenerPresion(9.7);
+
+        merlinNueve.obtenerTemperatura(70.7);
+        merlinNueve.obtenerPresion(9.7);
+
         boolean listo = false;
 
-        for (int i = 0; i <=8; i++){
-            if (merlinUno.temperatura <= 70.1 && merlinUno.temperatura >= 100.0 ) {
+        for (int i=0; i<objetos.length; i++) {
+
+            boolean estadoTemperatura = (objetos[i].temperatura >= 70.1) && (objetos[i].temperatura <= 100.0) ? true : false;
+            boolean estadoPresicion = (objetos[i].presion >= 9.0) && (objetos[i].presion <= 10.0) ? true : false;
+
+            if (estadoTemperatura && estadoPresicion) {
                 listo = !listo;
             } else {
-                System.out.println("Abortar. Fallo en el motor, temperatura alta: " + merlinUno.temperatura);
-            }
-            if (merlinUno.presion <= 9.0 && merlinUno.presion >= 10.0 ) {
-                listo = !listo;
-            } else {
-            System.out.println("Abortar. Fallo en el motor, presión alta: " + merlinUno.presion);
+                if (!estadoTemperatura) System.out.println("Abortar. Fallo en el motor " + objetos[i] + ", temperatura fuera de rango: " + objetos[i].temperatura + "°C");
+                if (!estadoPresicion) System.out.println("Abortar. Fallo en el motor " + objetos[i] + ", presión fuera de rango: " + objetos[i].presion + "KPa");
             }
         }
-        merlinUno.obtenerTemperatura(2.7f);
-        merlinUno.obtenerPresion(4.7f);
-        System.out.println(merlinUno.temperatura);
-        System.out.println(merlinUno.presion);
         return listo;
     }
 }
